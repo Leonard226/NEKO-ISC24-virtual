52e37ce235a889116c24c505598b878996af0979
Leonard Schmidt <125858470+Leonard226@users.noreply.github.com>
2024-05-24 17:38:15 +0200

    Update make.sh

--- GPU-VERSION/make.sh
+++ GPU-VERSION/make.sh
@@ -16,7 +16,20 @@ FLAGS_GFORTRAN="-O3 -march=znver3 -mtune=znver3 -funroll-loops"
 FLAGS_GCC="gcc -O3 -march=znver3 -mtune=znver3 -funroll-loops"
 FLAGS_NVCC="-O3 -allow-unsupported-compiler"
 
-./configure FCFLAGS="${FLAGS_GFORTRAN}" CFLAGS="-O3" CC="${FLAGS_GCC}" FC=gfortran LDFLAGS="-L${LAPACK_HOME}/lib -L/opt/nvidia/hpc_sdk/Linux_x86_64/24.3/comm_libs/nccl/lib" --with-parmetis=${Parmetis_HOME} --with-metis=${metis_HOME} --with-cuda=${CUDA_HOME} CUDA_ARCH=-arch=sm_80 NVCC=/opt/nvidia/hpc_sdk/Linux_x86_64/24.3/compilers/bin/nvcc CUDA_CFLAGS="${FLAGS_NVCC}" --enable-device-mpi --with-nccl=/opt/nvidia/hpc_sdk/Linux_x86_64/24.3/comm_libs/nccl --prefix=${P_Neko_Install}
+./configure FCFLAGS="${FLAGS_GFORTRAN}" \ 
+            CFLAGS="-O3" \
+            CC="${FLAGS_GCC}" \
+            FC=gfortran \
+            LDFLAGS="-L${LAPACK_HOME}/lib -L/opt/nvidia/hpc_sdk/Linux_x86_64/24.3/comm_libs/nccl/lib" \
+            --with-parmetis=${Parmetis_HOME} \
+            --with-metis=${metis_HOME} \
+            --with-cuda=${CUDA_HOME} \
+            CUDA_ARCH=-arch=sm_80 \
+            NVCC=/opt/nvidia/hpc_sdk/Linux_x86_64/24.3/compilers/bin/nvcc \
+            CUDA_CFLAGS="${FLAGS_NVCC}" \
+            --enable-device-mpi --with-nccl=/opt/nvidia/hpc_sdk/Linux_x86_64/24.3/comm_libs/nccl \
+            --prefix=${P_Neko_Install}
+
 rm -rf ${P_Neko_Install}
 mkdir -p ${P_Neko_Install}
 make -j32 && make -j32 install
